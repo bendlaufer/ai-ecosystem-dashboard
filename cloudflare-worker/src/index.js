ae6d164ef04a5467f3897ebca55158809cb61d87
bendlaufer <ben.laufer@gmail.com>
2025-12-10 17:42:58 -0500

    Fix Worker streaming - remove Content-Encoding header
    - Removed Content-Encoding: gzip to prevent browser auto-decompression conflict
    - Added Content-Length header for better streaming
    - Browser will manually decompress using DecompressionStream API
    - Fixes 'Unexpected end of JSON input' error

--- cloudflare-worker/src/index.js
+++ cloudflare-worker/src/index.js
@@ -22,18 +22,25 @@ export default {
     // Stream the gzipped file directly (don't decompress - let browser handle it)
     // This avoids Worker timeout/memory issues with large files
     const body = object.body;
+    const size = object.size; // Get file size from R2 object
+
+    // Build headers
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+      'Content-Type': 'application/gzip',
+      'Cache-Control': 'public, max-age=3600',
+    };
+    
+    // Add Content-Length if available (helps with streaming)
+    if (size) {
+      headers['Content-Length'] = size.toString();
+    }
 
     // Return gzipped file with CORS headers
     // Browser will decompress using DecompressionStream API
-    return new Response(body, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
-        'Content-Type': 'application/gzip',
-        'Content-Encoding': 'gzip',
-        'Cache-Control': 'public, max-age=3600',
-      },
-    });
+    // Don't set Content-Encoding: gzip - let browser handle decompression manually
+    return new Response(body, { headers });
   },
 };
 
